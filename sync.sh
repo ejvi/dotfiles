c15d9673ac4e9a45ef95e2957686bc11571c3def
ejvi <evolutionsup@gmail.com>
2022-04-25 19:55:01 +0300

    $c

---
+++ sync.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+
+cp -r -v ~/.config/dunst ~/dotfiles
+cp -r -v ~/.config/i3 ~/dotfiles
+cp -r -v ~/.config/polybar ~/dotfiles
+cp -r -v ~/.config/rofi ~/dotfiles
+cp -r -v ~/.config/picom.conf ~/dotfiles
+cp -r -v ~/sync.sh ~/dotfiles
+
+echo -e "${RED}DOTFILES COPIED"
+
+cd ~/dotfiles
+git add -A
+read -p 'ENTER COMMIT: ' c
+git commit -m '$c'
+git push -u -f origin master
+
+echo -e "${GREEN}DOTFILES PUSHED"
